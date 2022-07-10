cd01d6618aaaf6574af1cf533208884a56dba0fd
Jamel Yassin <jamelyassin84@gmail.com>
2022-07-10 12:41:32 +0400

    changes

--- src/app/app-core/constants/app.constants.ts
+++ src/app/app-core/constants/app.constants.ts
@@ -2,12 +2,78 @@ export const SEMESTERS = ['1st', '2nd', '3rd']
 
 export const YEAR_LEVELS = ['1st', '2nd', '3rd', '4th', '5th']
 
-export const DEGREES = ['Bachelor of Arts','Bachelor of Science in Agriculture', 'Bachelor of Science in Agricultural and Biosystems Engineering',
- 'Bachelor of Science in Environmental Management', 'Bachelor of Science in Accountancy', 'Bachelor of Science in Advertising',
- 'Bachelor of Science in Computer Science','Bachelor of Science in Digital Media and Interactive Arts', 'Bachelor of Science in Information Technology']
+export const DEGREES = [
+	'Bachelor of Arts',
+	'Bachelor of Science in Agriculture',
+	'Bachelor of Science in Agricultural and Biosystems Engineering',
+	'Bachelor of Science in Environmental Management',
+	'Bachelor of Science in Accountancy',
+	'Bachelor of Science in Advertising',
+	'Bachelor of Science in Computer Science',
+	'Bachelor of Science in Digital Media and Interactive Arts',
+	'Bachelor of Science in Information Technology',
+]
 
-export const DEPARTMENTS = ['CCS', 'CBA', 'CHM', 'CN', 'CE','CAS','CARES','CMLS','CP','CL','CM','CT',]
+export const DEPARTMENTS = [
+	'CCS',
+	'CBA',
+	'CHM',
+	'CN',
+	'CE',
+	'CAS',
+	'CARES',
+	'CMLS',
+	'CP',
+	'CL',
+	'CM',
+	'CT',
+]
 
-export const COURSES = ['Computer Science', 'Information Technology','Digital Media and Interactive Arts','Library and Information Science','Communication',
-'English Language Studies','Political Science','Biology','Biology with specialization in Microbiology','Chemistry',
-'Psychology','Social Work','Major in Political Science and Public Administration','Major in Mathematics']
+export const COURSES = [
+	'Computer Science',
+	'Information Technology',
+	'Digital Media and Interactive Arts',
+	'Library and Information Science',
+	'Communication',
+	'English Language Studies',
+	'Political Science',
+	'Biology',
+	'Biology with specialization in Microbiology',
+	'Chemistry',
+	'Psychology',
+	'Social Work',
+	'Major in Political Science and Public Administration',
+	'Major in Mathematics',
+]
+
+export const DEGREES1 = [
+	{
+		name: 'Bachelor of Arts', // Name of Degree
+		departments: [
+			{
+				name: 'CCS', // Name of Department
+				courses: [
+					'English Language Studies',
+					'Political Science',
+					'Biology',
+					'Biology with specialization in Microbiology',
+					'Chemistry',
+					'Psychology',
+					'Social Work',
+				],
+			},
+			{
+				name: 'CBA',
+				courses: [
+					'English Language Studies',
+					'Political Science',
+					'Biology',
+					'Biology with specialization in Microbiology',
+					'Chemistry',
+					'Psychology',
+					'Social Work',
+				],
+			},
+		],
+	},
+]
