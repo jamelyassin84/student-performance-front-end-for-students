68cd63698bbd91b56c5ae826239057715aeba35b
Jamel Yassin <jamelyassin84@gmail.com>
2022-07-17 19:50:26 +0400

    changes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -16,13 +16,11 @@ export const appRoutes: Route[] = [
 	// After the user signs in, the sign in page will redirect the user to the 'signed-in-redirect'
 	// path. Below is another redirection for that path to redirect the user to the desired
 	// location. This is a small convenience to keep all main routes together here on this file.
-	{ path: 'signed-in-redirect', pathMatch: 'full', redirectTo: 'dashboard' },
+	// { path: 'signed-in-redirect', pathMatch: 'full', redirectTo: 'sign-in' },
 
 	// Auth routes for guests
 	{
 		path: '',
-		canActivate: [NoAuthGuard],
-		canActivateChild: [NoAuthGuard],
 		component: LayoutComponent,
 		data: {
 			layout: 'empty',
