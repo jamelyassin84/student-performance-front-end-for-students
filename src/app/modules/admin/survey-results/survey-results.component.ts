4562d39782c8e465fadc21d76c6ee84a64e1b394
Jamel Yassin <jamelyassin84@gmail.com>
2022-07-24 18:54:15 +0400

    changes

--- src/app/modules/admin/survey-results/survey-results.component.ts
+++ src/app/modules/admin/survey-results/survey-results.component.ts
@@ -1,5 +1,10 @@
+import { StudentPerformance } from './../../../app-core/store/performance/performance.model'
 import { Component, OnInit } from '@angular/core'
 import { SEMESTERS, YEAR_LEVELS } from 'app/app-core/constants/app.constants'
+import { SurveyPerformanceService } from 'app/app-core/store/performance/performance.service'
+import { StudentService } from 'app/app-core/services/student.service'
+import { take } from 'rxjs'
+import { toImplicitRating } from '@global_packages/helpers/helpers'
 
 @Component({
 	selector: 'app-survey-results',
@@ -7,12 +12,30 @@ import { SEMESTERS, YEAR_LEVELS } from 'app/app-core/constants/app.constants'
 	styleUrls: ['./survey-results.component.scss'],
 })
 export class SurveyResultsComponent implements OnInit {
-	SEMESTERS = SEMESTERS
-	YEAR_LEVELS = YEAR_LEVELS
+	constructor(
+		private _studentService: StudentService,
+		private _surveyPerformanceService: SurveyPerformanceService,
+	) {}
 
-	constructor() {}
+	performances: StudentPerformance[] = []
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		this.getPerformances()
+	}
 
 	identity = (item: any) => item
+
+	getPerformances() {
+		this._studentService.student$.pipe(take(1)).subscribe((student) => {
+			this._surveyPerformanceService
+				.query(`?student_id=${student.id}`)
+				.subscribe((performances) => {
+					this.performances = performances
+				})
+		})
+	}
+
+	toImplicitRating(performance: number) {
+		return toImplicitRating(performance)
+	}
 }
